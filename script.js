ad4afe194f52559b69f4598798b23d4747bffbc4
Reda Khaldi <raidenkratos@gmail.com>
2020-09-14 21:28:47 +0200

    coding snake5

--- script.js
+++ script.js
@@ -1,159 +1,140 @@
-//création du niveau
-const cvs = document.getElementById('snake');
-const ctx = cvs.getContext('2d');
+
+
+const cvs = document.getElementById("snake");
+const ctx = cvs.getContext("2d");
+
 
 const box = 32;
 
-const ground  = new Image ();
-ground.src="ground.png";
+// charger l'img
 
-const food = new Image();
-food.src="food.png";
- 
-const dead = new Audio();
-const eat = new Audio();
-const up = new Audio();
-const left = new Audio();
-const right = new Audio();
-const down = new Audio();
+const ground = new Image();
+ground.src = "img/ground.png";
 
-dead.src="";
-eat.src="";
-up.src="";
-left.src="";
-right.src="";
-down.src="";
+const foodImg = new Image();
+foodImg.src = "img/food.png";
 
 
+// création du serpent
 
-
-ctx.drawImage(imageName,X,Y,Width,Height);
-ctx.drawImage("",40,50,25,25);
-ctx.fillStyle="red";
-ctx.fillRect(100,300,30,30);
-
-let box =32 
-ctx.fillStyle="black";
-ctx.fillRect=(5*box,6*box,2*box,3*box);
-
-//Création du serpent
 let snake = [];
-snake[0] = { x : 9*box,y : 10*box};
-//snake[1] = { x : 8*box,y : 10*box};
-////création de la nourriture
 
-let food ={
-    x: Math.floor(Math.random()*17+1)*box,
-    y: Math.floor(Math.random()*15+3)*box
-
-
+snake[0] = {
+    x : 9 * box,
+    y : 10 * box
 };
-//Score du game
-let score=0;
 
-function draw () {
-    ctx.drawImage(ground,0,0);
-     for (let i = 0; i < snake.length; i++) {
-        const element = array[index];
-        ctx.fillStyle = (i==0 )?"green":"white";
-        ctx.fillRect(snake[i].x,snake[i].y,box,box);
-        ctx.strokeStyle="red";
-        ctx.strokeRect(snake[i].x,snake[i].y,box,box);
-        
-    }
-    ctx.drawImage(foodImg,food.x,food.y);
-    ctx.fillStyle="White";
-    ctx.font="45px Changa One";
-    ctx.fillText(score,2*box,1.6*box);
+// creer la nourriture
+
+let food = {
+    x : Math.floor(Math.random()*17+1) * box,
+    y : Math.floor(Math.random()*15+3) * box
 }
-let game = setInterval(draw,100);
 
-//Controle du serpent
+// creation score
+
+let score = 0;
+
+//controle du serpent
+
 let d;
+
 document.addEventListener("keydown",direction);
 
-function direction(event) {
-    if (event.keyCode == 37 && d!="RIGHT" ) {
+function direction(event){
+    let key = event.keyCode;
+    if( key == 37 && d != "RIGHT"){
         left.play();
-        d="LEFT";
-        
-    } else if(event.keyCode==38 && d!="DOWN") { 
+        d = "LEFT";
+    }else if(key == 38 && d != "DOWN"){
+        d = "UP";
         up.play();
-
-        d="UP";
-        
-    }else if(event.keyCode==40&& d!="UP") { 
-        down.play();
-
-        d="DOWN";
-        
-    }else if(event.keyCode==39 && d!="LEFT") { 
+    }else if(key == 39 && d != "LEFT"){
+        d = "RIGHT";
         right.play();
-
-        d="RIGHT";
-        
+    }else if(key == 40 && d != "UP"){
+        d = "DOWN";
+        down.play();
     }
-    
 }
 
-snake.pop();
-snakeX=snake[0].x;
-snakeY=snake[0].y;
-
-
-if (d="LEFT") {
-    snakeX-=box;
-
-}
-if (d="UP") {
-    snakeY-=box;
-
-    
-}if (d="RIGHT") {
-    snakeX+=box;
-
-}
-if (d="DOWN") {
-    snakeY+=box;
-
-}
-
-
-
-let newHead= {
-    x:snakeX,
-    y:snakeY
-
-}
-snake.unshift(newHead);
-
-if (snakeX==food.x && snakeY==food.y) {
-    score++;
-    eat.play();
-    food={
-        x:unit*Math.floor(Math.random()*17+1),
-        y:unit*Math.floor(Math.random()*15+3)
-    }
-    //On ajoute newhead sans le snake.pop
-    
-} else {
-    snake.pop();
-}
-
-function collision(head,array) {
-    
-    for ( let i=0;i<snake.length;i++) {
-       if (newHead.x==snake[i].x && newHead.y == snake[i].y) {
-           return true;
-           
-       }
-        
+// verification de la fonction collision
+function collision(head,array){
+    for(let i = 0; i < array.length; i++){
+        if(head.x == array[i].x && head.y == array[i].y){
+            return true;
+        }
     }
     return false;
 }
-//game over 
-if (snakeX<box || snakeX>17*box ||snakeY<3*box ||snakeY>17*box || collision(newHead,snake) ) {
-    clearInterval(game);
-    dead.play();
+
+// dessine le niv dans le canvas
+
+function draw(){
+    
+    ctx.drawImage(ground,0,0);
+    
+    for( let i = 0; i < snake.length ; i++){
+        ctx.fillStyle = ( i == 0 )? "green" : "white";
+        ctx.fillRect(snake[i].x,snake[i].y,box,box);
+        
+        ctx.strokeStyle = "red";
+        ctx.strokeRect(snake[i].x,snake[i].y,box,box);
+    }
+    
+    ctx.drawImage(foodImg, food.x, food.y);
+    
+    //  position tete serpent
+    let snakeX = snake[0].x;
+    let snakeY = snake[0].y;
+    
+    // quel position
+    if( d == "LEFT") snakeX -= box;
+    if( d == "UP") snakeY -= box;
+    if( d == "RIGHT") snakeX += box;
+    if( d == "DOWN") snakeY += box;
+    
+    // si le snake mange la nourriture
+    if(snakeX == food.x && snakeY == food.y){
+        score++;
+        food = {
+            x : Math.floor(Math.random()*17+1) * box,
+            y : Math.floor(Math.random()*15+3) * box
+        }
+        // on enleve pas la queue
+    }else{
+        // enleve la queue
+        snake.pop();
+    }
+    
+    // ajoute une tete
+    
+    let newHead = {
+        x : snakeX,
+        y : snakeY
+    }
+    
+    // game over
+    
+    if(snakeX < box || snakeX > 17 * box || snakeY < 3*box || snakeY > 17*box || collision(newHead,snake)){
+        clearInterval(game);
+    }
+    
+    snake.unshift(newHead);
+    
+    ctx.fillStyle = "white";
+    ctx.font = "45px Changa one";
+    ctx.fillText(score,2*box,1.6*box);
 }
+
+// appel la fonction draw tous les 100 ms
+
+let game = setInterval(draw,100);
+
+
+
+
+
+
+
 
